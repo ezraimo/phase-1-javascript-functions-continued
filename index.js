16f724d9fc078d8b33c336658469b325c6f36c80
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-09 15:40:06 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // code your solution here
+const hqLocation = 42;
+const feetPerBlock = 264;
+
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * feetPerBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair) {
+  return function(adjective) {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+module.exports = {
+  distanceFromHqInBlocks,
+  distanceFromHqInFeet,
+  distanceTravelledInFeet,
+  calculatesFarePrice,
+  saturdayFun,
+  mondayWork,
+  wrapAdjective,
+};
+
